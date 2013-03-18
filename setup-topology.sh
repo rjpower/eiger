be9f527048a4562eb106e758d6ee4241684bce4d
Russell Power <power@cs.nyu.edu>
2013-03-18 19:27:33 +0000

    Update scripts

--- setup-topology.sh
+++ setup-topology.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+HOSTS=$(ec2-describe-instances | grep INSTANCE | awk '{print $15'})
 cp conf/cassandra-topology.TEMPLATE  conf/cassandra-topology.properties
-ec2-describe-instances | grep INSTANCE | awk '{print $15"=DC1:RAC1"}' >> conf/cassandra-topology.properties
 
+echo $HOSTS | xargs -n1 | awk '{print $1"=DC1:RAC1"}' >> conf/cassandra-topology.properties
+SEED=$(echo $HOSTS | xargs -n1 | tail -n1)
+sed  -e "s/SEED_HOST/$SEED/" conf/cassandra.yaml.TEMPLATE  > conf/cassandra.yaml
 
