3ef0301825cc4567249d32f644657b862d5864b4
Russell Power <power@cs.nyu.edu>
2013-03-19 20:51:25 +0000

    Some more scripts.

--- ec2/ec2-utils.sh
+++ ec2/ec2-utils.sh
@@ -12,3 +12,16 @@ function get_public_ips() {
   ( ec2-describe-instances --region $region | grep INSTANCE | grep running | awk -F'[\t]' '{print $17}') | tee ec2-public-ips.$region
 }
 
+function get_region() {
+ wget -qO- http://instance-data/latest/dynamic/instance-identity/document | 
+  grep availabilityZone | 
+  awk -F'"' '{print $4}' | 
+  awk -F- '{print $1 "-" $2}'
+}
+
+function get_name() {
+  ec2-describe-tags \
+    --filter "resource-type=instance" \
+    --filter "resource-id=$($(dirname $0)/ec2-metadata -i | cut -d ' ' -f2)" \
+    --filter "key=Name" | cut -f5
+}
