a5366525aeaf71e66240a14732019b798e02c2f2
Russell Power <power@cs.nyu.edu>
2013-03-21 13:19:37 -0400

    Use network topology strategy?

--- ec2/run-stress.sh
+++ ec2/run-stress.sh
@@ -10,12 +10,32 @@ OFFSET=$((OFFSET - 1))
 
 TOTAL=$(cat ec2-info* | grep $SERVER_TAG | wc -l)
 
-echo My offset: $OFFSET, total client: $TOTAL
+num_dcs=0
+for r in $REGIONS; do
+  if (grep $SERVER_TAG ec2-info.$r); then
+    num_dcs=$((num_dcs + 1))
+  fi
+done
+ 
+strategy_properties="DC0:1"
+for i in $(seq 1 $((num_dcs-1))); do
+  strategy_properties=$(echo ${strategy_properties}",DC${i}:1")
+done
+
+echo My offset: $OFFSET, total client: $TOTAL, strategy: $strategy_properties
+
+exit
 
 DIRNAME=$(readlink -f .)
 rsync -e 'ssh -oStrictHostKeyChecking=no' -aip nfs.rjpower.org:$DIRNAME /tmp/
 (
   cd /tmp/eiger/tools/stress/
   ant
-  bin/stress --nodes=localhost --stress-index=$OFFSET --stress-count=$TOTAL $*
+  bin/stress \
+  --nodes=localhost \
+  --stress-index=$OFFSET \
+  --stress-count=$TOTAL \
+  --replication-strategy=NetworkTopologyStrategy \
+  --strategy-properties=$strategy_properties \
+  $*
 )
