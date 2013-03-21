2c643030032cd135e027bee705fb12bdfa2d09a7
Russell Power <power@cs.nyu.edu>
2013-03-21 16:33:52 +0000

    Yay, cassandra can't actually bootstrap itself.

--- ec2/run-eiger.sh
+++ ec2/run-eiger.sh
@@ -7,11 +7,38 @@ TMP=$(cat ec2-info.*)
 [[ -n $TMP ]] || abort "Setup topology has not been run."
 
 # the first server in our list is the seed.
-SEEDS=$(cat ec2-info.* | grep $SERVER_TAG | head -n1 | awk '{printf("        - seeds: " $4 "\\n")}')
+SEED=$(cat ec2-info.* | grep $SERVER_TAG | head -n1 | awk '{print $4}')
+SEEDS="        - seeds: $SEED\\n"
+
 PUBLIC=$(wget -qO- http://instance-data/latest/meta-data/public-ipv4)
 PRIVATE=$(wget -qO- http://instance-data/latest/meta-data/local-ipv4)
+TOKEN=
 
-echo Public ip: $PUBLIC, private ip: $PRIVATE
+# we need to set our initial_token, because cassandra is borked.
+# find the datacenter we belong to
+for dc in $(seq 10); do 
+  if (cat conf/cassandra-topology.properties | grep "DC$dc" | grep $PUBLIC); then
+    # what offset are we at in our datacenter?
+    offset=$(cat conf/cassandra-topology.properties | grep "DC$dc" | egrep -v '^10.' | grep -n $PUBLIC | awk -F: '{print $1}')
+    offset=$((offset - 1))
+
+    # how many nodes in our datacenter?
+    dc_nodes=$(cat conf/cassandra-topology.properties | grep "DC$dc" | egrep -v '^10.' | wc -l)
+
+    # our token is $dc + 2**127 * offset / dc_nodes
+    bc_cmd="$offset*(2^127)/$dc_nodes + $dc"
+    TOKEN=$(echo $bc_cmd | bc)
+    echo "My dc: $dc, offset: $offset, token $TOKEN"
+  fi
+done
+
+[[ -n $TOKEN ]] || abort "Failed to find self in properties file"
+
+echo "Blowing away database...."
+rm -rf /tmp/cassandra
+
+echo Public ip: $PUBLIC, private ip: $PRIVATE, seed: $SEED
+echo Seed line: $SEEDS
 
 DIRNAME=$(readlink -f .)
 
@@ -21,6 +48,7 @@ rsync -e 'ssh -oStrictHostKeyChecking=no' -aip nfs.rjpower.org:$DIRNAME /tmp/
   cat conf/cassandra.yaml.TEMPLATE |
     sed -e "s/PUBLIC_ADDRESS/$PUBLIC/g" |
     sed -e "s/PRIVATE_ADDRESS/$PRIVATE/g" |
+    sed -e "s/INITIAL_TOKEN/$TOKEN/g" |
     sed -e "s/SEED_HOST/$SEEDS/g" > conf/cassandra.yaml
   bin/cassandra $*
 )
--- ec2/setup-topology.sh
+++ ec2/setup-topology.sh
@@ -3,15 +3,13 @@
 source $(dirname $0)/ec2-utils.sh
 
 cp conf/cassandra-topology.TEMPLATE conf/cassandra-topology.properties
-DC=1
 for r in $REGIONS; do 
   (list_all $r) &
 done
 wait
 
+DC=1
 for r in $REGIONS; do  
-  grep $SERVER_TAG ./ec2-info.$r | 
-    awk "{print \$3\"=DC$DC:RAC1\"; print \$4\"=DC$DC:RAC1\"}" 
-    >> conf/cassandra-topology.properties
+  grep $SERVER_TAG ./ec2-info.$r | awk "{print \$3\"=DC$DC:RAC1\"; print \$4\"=DC$DC:RAC1\"}" >> conf/cassandra-topology.properties
   DC=$((DC + 1))
 done
